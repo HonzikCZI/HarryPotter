e668daf168cc6ec2e1998c87f9241e97f3e500f7
HonzikCZI <jan.sara.cze@gmail.com>
2024-07-25 09:51:12 +0200

    init

---
+++ main.py
@@ -0,0 +1,30 @@
+import pygame   
+
+# Inicializace hry
+pygame.init()
+
+# Obrazovka
+Width = 1000
+height = 500
+screen = pygame.display.set_mode((Width, height))
+pygame.display.set_caption("Harry Potter a Ohnivý pohár")
+
+# Nastaveni hry
+
+# FPS a hodiny
+
+# Barvy
+
+# Fonty
+
+# Zvuky a muzika v pozadi
+
+# Obrazky
+
+# Hlavni ciklus
+lets_continue = True
+while lets_continue:
+    
+
+# Ukonceni hry
+pygame.quit()
